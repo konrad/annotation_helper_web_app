2faed008d5217b71de3443dfabef6e443ecf16ba
Konrad Förstner <konrad@foerstner.org>
2013-05-13 18:03:32 +0200

    Add modification time

--- annotate.py
+++ annotate.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template, redirect
 import json
+import datetime
+
 app = Flask(__name__)
 data_file = "list_annotation.json"
 
@@ -14,6 +16,7 @@ def show(entity_id):
         'show.html', 
         entity_id=entity_id,
         status=features["status"],
+        mod_time=features["mod_time"],
         confirm_url=url_for("confirm", entity_id = entity_id),
         reject_url=url_for("reject", entity_id = entity_id))
 
@@ -29,7 +32,7 @@ def reject(entity_id):
 
 def _get_features(entity_id):
     entities = _entities()
-    return entities.get(entity_id, {"status" : "Undefined"})
+    return entities.get(entity_id, {"status" : "Undefined", "mod_time" : ""})
 
 def _entities():
     try:
@@ -42,9 +45,12 @@ def _entities():
 
 def _save_annotation(entity_id, status):
     entities = _entities()
-    entities[entity_id] = {"status" : status, "mod_time" : ""}
+    entities[entity_id] = {"status" : status, "mod_time" : _now_str()}
     with open(data_file, "w") as fh:
         json.dump(entities, fh)
+
+def _now_str():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 if __name__ == "__main__":
     app.debug = True
