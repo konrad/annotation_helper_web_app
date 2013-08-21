b9f62e382770987489afa512650c2dd63150eabd
Konrad Förstner <konrad@foerstner.org>
2013-08-21 08:32:44 +0200

    Add offset and option to change it

--- annotate.py
+++ annotate.py
@@ -50,28 +50,45 @@ def annotate(entity_id):
         "annotate.html", 
         entity_id=entity_id,
         status=features["status"],
+        offset=features["offset"],
         mod_time=features["mod_time"],
-        confirm_url=url_for("confirm", entity_id = entity_id),
-        reject_url=url_for("reject", entity_id = entity_id),
+        confirm_url=url_for("confirm", entity_id=entity_id),
+        reject_url=url_for("reject", entity_id=entity_id),
+        offset_min_3_url=_offset_url(entity_id, -3),
+        offset_min_2_url=_offset_url(entity_id, -2),
+        offset_min_1_url=_offset_url(entity_id, -1),
+        offset_0_url=_offset_url(entity_id, 0),
+        offset_plus_1_url=_offset_url(entity_id, 1),
+        offset_plus_2_url=_offset_url(entity_id, 2),
+        offset_plus_3_url=_offset_url(entity_id, 3),
         list_all_url=url_for("list_all"))
+
+def _offset_url(entity_id, offset):
+    return url_for("add_offset", entity_id=entity_id, offset=offset)
 
 @app.route("/confirm/<entity_id>")
 def confirm(entity_id):
-    _save_annotation(entity_id, "confirmed")
+    _save_annotation(entity_id, status="confirmed")
     return redirect(url_for("annotate", entity_id = entity_id))
 
 @app.route("/reject/<entity_id>")
 def reject(entity_id):
-    _save_annotation(entity_id, "rejected")
+    _save_annotation(entity_id, status="rejected")
     return redirect(url_for("annotate", entity_id = entity_id))
 
 @app.route("/listall")
 def list_all():
     return render_template("list_all.html", entities = _entities())
 
+@app.route("/add_offset/<entity_id>/<offset>")
+def add_offset(entity_id, offset):
+    _save_annotation(entity_id, offset=int(offset))
+    return redirect(url_for("annotate", entity_id = entity_id))
+
 def _get_features(entity_id):
     entities = _entities()
-    return entities.get(entity_id, {"status" : "Undefined", "mod_time" : ""})
+    return entities.get(entity_id, {"status" : "Undefined", "offset" : 0, 
+                                    "mod_time" : ""})
 
 def _entities():
     try:
@@ -82,9 +99,18 @@ def _entities():
             fh.write("{}")
             return {}
 
-def _save_annotation(entity_id, status):
+def _save_annotation(entity_id, status=None, offset=None):
     entities = _entities()
-    entities[entity_id] = {"status" : status, "mod_time" : _now_str()}
+    if entity_id in entities:
+        if status is None:
+            status = entities[entity_id].get("status", "Undefined")
+        if offset is None:
+            offset = entities[entity_id].get("offset", 0)
+    else:
+        status = "Undefined"
+        offset = 0
+    entities[entity_id] = {"status" : status, "mod_time" : _now_str(), 
+                           "offset" : offset}
     with open(data_file, "w") as fh:
         json.dump(entities, fh)
 
