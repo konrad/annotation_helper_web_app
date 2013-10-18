7c77fbadbfd73c638e0dde9e7304a7b1039c2d3a
Konrad Förstner <konrad@foerstner.org>
2013-10-18 08:40:38 +0200

    Move license to seperate file

--- annotate.py
+++ annotate.py
@@ -7,22 +7,6 @@ Test e.g. with
 
 http://127.0.0.1:5000/annotate/a_test_gene
 
-Copyright (c) 2013, Konrad Foerstner <konrad@foerstner.org>
-
-Permission to use, copy, modify, and/or distribute this software for
-any purpose with or without fee is hereby granted, provided that the
-above copyright notice and this permission notice appear in all
-copies.
-
-THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL
-WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
-PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-
 """
 __description__ = ""
 __author__ = "Konrad Foerstner <konrad@foerstner.org>"
@@ -116,8 +100,10 @@ def _save_annotation(entity_id, status=None, offset=None):
         if offset is None:
             offset = entities[entity_id].get("offset", 0)
     else:
-        status = "Undefined"
-        offset = 0
+        if status is None:
+            status = "Undefined"
+        if offset is None:
+            offset = 0
     entities[entity_id] = {"status" : status, "mod_time" : _now_str(), 
                            "offset" : offset}
     with open(data_file, "w") as fh:
